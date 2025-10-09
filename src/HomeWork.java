22c27047f656f68cbcf08f9a0c2f4e612ed7f894
FeyziyevEmin <feyziyevemin2006@gmail.com>
2025-10-09 18:01:44 +0400

    first commit

---
+++ src/HomeWork.java
@@ -0,0 +1,12 @@
+public class HomeWork {
+    public static void main(String[]args){
+       int a=10;
+       Integer b=a;
+       Integer c=100;
+       int d=c;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+    }
+}
